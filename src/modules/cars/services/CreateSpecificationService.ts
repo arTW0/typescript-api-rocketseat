4130bf0d44afc541472c40f879147c8374d4e4e3
Arthur <arthur.moreira@itec.ufpa.br>
2023-07-28 16:39:17 -0300

    19 - criando repositório de especificação

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,26 @@
-class CreateSpecificationService {
-  execute() {
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository"
 
+interface IRequest {
+  name: string;
+  description: string;
+}
+class CreateSpecificationService {
+  static execute(arg0: { name: any; description: any; }) {
+    throw new Error('Method not implemented.');
+  }
+  constructor(private specificationRepository: ISpecificationsRepository) { }
+
+  execute({ description, name }: IRequest): void {
+    const specificationAlreadyExists = this.specificationRepository.findByName(name)
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists")
+    }
+
+    this.specificationRepository.create({
+      description,
+      name
+    })
   }
 }
 
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService'
+import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository'
+
+const specificationsRoutes = Router()
+const specificationRepository = new SpecificationsRepository()
+
+specificationsRoutes.post('/', (req, res) => {
+  const { name, description } = req.body
+  const createSpecificationService = new CreateSpecificationService(specificationRepository)
+
+  createSpecificationService.execute({ name, description })
+
+  return res.status(201).send()
+})
+
+
+export { specificationsRoutes }
